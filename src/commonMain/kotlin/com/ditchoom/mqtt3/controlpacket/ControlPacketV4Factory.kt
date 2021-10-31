b8112adfb154a94f7c970a00c06a5eb95161c74a
Rahul Behera <thebehera@fb.com>
2021-10-30 23:17:58 -0700

    add simple subscription in factory

--- src/commonMain/kotlin/com/ditchoom/mqtt3/controlpacket/ControlPacketV4Factory.kt
+++ src/commonMain/kotlin/com/ditchoom/mqtt3/controlpacket/ControlPacketV4Factory.kt
@@ -6,6 +6,7 @@ import com.ditchoom.buffer.PlatformBuffer
 import com.ditchoom.buffer.ReadBuffer
 import com.ditchoom.mqtt.controlpacket.*
 import com.ditchoom.mqtt.controlpacket.format.ReasonCode
+import com.ditchoom.mqtt.topic.Filter
 
 object ControlPacketV4Factory : ControlPacketFactory {
 
@@ -35,6 +36,25 @@ object ControlPacketV4Factory : ControlPacketFactory {
         val fixedHeader = PublishMessage.FixedHeader(dup, qos, retain)
         val variableHeader = PublishMessage.VariableHeader(topicName, packetIdentifier)
         return PublishMessage(fixedHeader, variableHeader, payload)
+    }
+
+    override fun subscribe(
+        packetIdentifier: Int,
+        topicFilter: CharSequence,
+        maximumQos: QualityOfService,
+        noLocal: Boolean,
+        retainAsPublished: Boolean,
+        retainHandling: ISubscription.RetainHandling,
+        serverReference: CharSequence?,
+        userProperty: List<Pair<CharSequence, CharSequence>>
+    ): ISubscribeRequest {
+        val subscription = Subscription(Filter(topicFilter), maximumQos)
+        return subscribe(
+            packetIdentifier,
+            setOf(subscription),
+            serverReference,
+            userProperty
+        )
     }
 
     override fun subscribe(
